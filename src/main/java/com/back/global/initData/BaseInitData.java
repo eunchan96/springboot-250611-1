18e4430140b7d54eed6994ad858ceac9551b1a7f
eunchan96 <taegwon02@gmail.com>
2025-06-12 14:20:10 +0900

    PostService

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,7 +1,7 @@
 package com.back.global.initData;
 
-import com.back.domain.post.post.Repository.PostRepository;
 import com.back.domain.post.post.entity.Post;
+import com.back.domain.post.post.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +12,7 @@ import java.util.Optional;
 @Configuration
 public class BaseInitData {
     @Autowired
-    private PostRepository postRepository;
+    private PostService postService;
 
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
@@ -22,16 +22,16 @@ public class BaseInitData {
         };
     }
     void work1() {
-        if(postRepository.count() > 0) return; // SELECT COUNT(*) FROM post;
+        if(postService.count() > 0) return; // SELECT COUNT(*) FROM post;
 
-        Post post1 = postRepository.save(new Post("제목 1", "내용 1")); // INSERT INTO post SET title = '제목 1';
-        Post post2 = postRepository.save(new Post("제목 2", "내용 2"));
+        Post post1 = postService.save(new Post("제목 1", "내용 1")); // INSERT INTO post SET title = '제목 1';
+        Post post2 = postService.save(new Post("제목 2", "내용 2"));
 
         System.out.println("기본 데이터가 초기화되었습니다.");
     }
 
     void work2() {
-        Optional<Post> opPost1 = postRepository.findById(1); // SELECT * FROM post WHERE id = 1;
+        Optional<Post> opPost1 = postService.findById(1); // SELECT * FROM post WHERE id = 1;
 
         Post post1 = opPost1.get();
 
