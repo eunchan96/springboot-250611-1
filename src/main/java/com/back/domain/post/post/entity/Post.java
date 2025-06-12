d422dfeb346e436e1346ae34bff486e4723fd37d
eunchan96 <taegwon02@gmail.com>
2025-06-12 14:10:45 +0900

    datetime

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -5,27 +5,30 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Post {
     @Id
     @GeneratedValue(strategy = IDENTITY)
-    private final int id;
+    private int id;
+    private LocalDateTime createDate;
+    private LocalDateTime modifiedDate;
     private String title;
     @Column(columnDefinition = "TEXT")
     private String content;
 
-    public Post() {
-        this("", "");
-    }
-
     public Post(String title, String content) {
-        this.id = 0;
+        this.createDate = LocalDateTime.now();
+        this.modifiedDate = this.createDate;
         this.title = title;
         this.content = content;
     }
