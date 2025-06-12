30b35be9278e6d81cf68931da188482683565927
eunchan96 <taegwon02@gmail.com>
2025-06-12 18:43:13 +0900

    modify (실제로 변경되어야 modifyDate가 갱신되ìã도록 - 내부로직ver(주석))

--- src/main/java/com/back/domain/post/post/service/PostService.java
+++ src/main/java/com/back/domain/post/post/service/PostService.java
@@ -27,6 +27,24 @@ public class PostService {
     }
 
     public void modify(Post post, String title, String content) {
+        /*
+        boolean changed = false;
+
+        if (!post.getTitle().equals(title)) {
+            post.setTitle(title);
+            changed = true;
+        }
+
+        if (!post.getContent().equals(content)) {
+            post.setContent(content);
+            changed = true;
+        }
+
+        if (changed) post.setModifyDate(LocalDateTime.now());
+
+        postRepository.save(post);
+        */
+
         post.setTitle(title);
         post.setContent(content);
 
