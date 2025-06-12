c16e33cddd1961244308c62eef0fabd03a0c57eb
eunchan96 <taegwon02@gmail.com>
2025-06-12 11:24:53 +0900

    save, 데이터초기화

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,6 +1,7 @@
 package com.back.global.initData;
 
 import com.back.domain.post.post.Repository.PostRepository;
+import com.back.domain.post.post.entity.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -14,9 +15,17 @@ public class BaseInitData {
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
         return args -> {
-            System.out.println("기본 데이터가 초기화되었습니다.");
+             if(postRepository.count() > 0) return; // SELECT COUNT(*) FROM post;
 
-            postRepository.count(); // SELECT COUNT(*) FROM post;
+            Post post1 = new Post();
+            post1.setTitle("제목 1");
+            postRepository.save(post1); // INSERT INTO post SET title = '제목 1';
+
+            Post post2 = new Post();
+            post2.setTitle("제목 2");
+            postRepository.save(post2);
+
+            System.out.println("기본 데이터가 초기화되었습니다.");
         };
     }
 }
